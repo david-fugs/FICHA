180c4a241b9afba41fc30524c598d144c8157ab4
David <fulgore0142@gmail.com>
2025-03-27 16:31:38 -0500

    cambios en el delete

--- code/student/deleteSurvey.php
+++ code/student/deleteSurvey.php
@@ -6,46 +6,38 @@ include("../../conexion.php");
 $id = $_GET['id'];
 $campo = $_GET['campo'];
 
-if($campo == "prepost"){
+if ($campo == "prepost") {
     $tabla = 'prePostnatales';
     $id_campo = 'id_prePostnatales';
-    $ruta = 'viewSurveys.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "familiasalud"){
+    $ruta = 'viewSurveys.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "familiasalud") {
     $tabla = 'familiasalud';
     $id_campo = 'id_salud_familiaSalud';
-    $ruta = 'viewHealthFamilySurvey.php?num_doc_est=' . $_GET['num_doc_est'];
-    
-}
-else if($campo == "desempeno"){
+    $ruta = 'viewHealthFamilySurvey.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "desempeno") {
     $tabla = 'desempeno';
     $id_campo = 'id_desempeno';
-    $ruta = 'viewPerformanceSurvey.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "educacion"){
+    $ruta = 'viewPerformanceSurvey.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "educacion") {
     $tabla = 'educacion';
     $id_campo = 'id_educacion';
-    $ruta = 'viewEducationSurvey.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "entornohogar"){
+    $ruta = 'viewEducationSurvey.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "entornohogar") {
     $tabla = 'entornohogar';
     $id_campo = 'id_hog';
-    $ruta = 'viewEntornoHogar.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "preescolar"){
+    $ruta = 'viewEntornoHogar.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "preescolar") {
     $tabla = 'preescolar';
     $id_campo = 'id_preescolar';
-    $ruta = 'viewPreescolarSurvey.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "personal"){
+    $ruta = 'viewPreescolarSurvey.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "personal") {
     $tabla = 'personal';
     $id_campo = 'id_personal';
-    $ruta = 'viewPersonalSurvey.php?num_doc_est=' . $_GET['num_doc_est'];
-}
-else if($campo == "preguntas"){
+    $ruta = 'viewPersonalSurvey.php?num_doc_est='.$_GET['num_doc_est'];
+} else if ($campo == "preguntas") {
     $tabla = 'preguntas';
     $id_campo = 'id_preguntas';
-    $ruta = 'viewQuestionSurvey.php?num_doc_est=' . $_GET['num_doc_est'];
+    $ruta = 'viewQuestionSurvey.php?num_doc_est='.$_GET['num_doc_est'];
 }
 
 
@@ -53,14 +45,15 @@ if ($stmt = $mysqli->prepare("DELETE FROM $tabla WHERE $id_campo = ?")) {
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $stmt->close();
-    echo "<script>alert('Encuesta eliminada correctamente.'); window.location.href = ' 
-    $ruta';</script>";
-
+    echo "<script>alert('Encuesta eliminada correctamente.'); window.location.href = '$ruta';</script>";
 } else {
-    echo "<script>alert('Error al eliminar la encuesta.'); window.location.href = '
-    $ruta';</script>";
+    echo "<script>alert('Error al eliminar la encuesta.'); window.location.href = '$ruta';</script>";
 }
 
 
 $mysqli->close();
 ?>
+<center>
+    <a href="viewSurveys.php?num_doc_est=<?= $_GET['num_doc_est'] ?> "><img src='../../img/atras.png' width="72" height="72" title="Regresar" /></a>
+
+</center>
